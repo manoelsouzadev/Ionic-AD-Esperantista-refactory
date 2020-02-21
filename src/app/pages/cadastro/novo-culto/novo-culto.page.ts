001b70b9369700213284d20e39a43136b40bbc5e
manoelsouzadev <manoel.souza280@gmail.com>
2020-02-20 21:35:10 -0300

    Adicionado funcionalidade de tirar foto

--- src/app/pages/cadastro/novo-culto/novo-culto.page.ts
+++ src/app/pages/cadastro/novo-culto/novo-culto.page.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { SharedModalService } from '../../../shared/services/shared-modal/shared-modal.service';
-import { LoadingController } from '@ionic/angular';
-import { CultosSemanaisService } from '../secoes-cadastro/secao-cadastro-cultos/cultos-semanais/cultos-semanais.service';
-import { FirebaseService } from '../../../shared/services/firebase/firebase.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { SharedModalService } from "../../../shared/services/shared-modal/shared-modal.service";
+import { LoadingController } from "@ionic/angular";
+import { CultosSemanaisService } from "../secoes-cadastro/secao-cadastro-cultos/cultos-semanais/cultos-semanais.service";
+import { FirebaseService } from "../../../shared/services/firebase/firebase.service";
 
 @Component({
-  selector: 'app-novo-culto',
-  templateUrl: './novo-culto.page.html',
-  styleUrls: ['./novo-culto.page.scss']
+  selector: "app-novo-culto",
+  templateUrl: "./novo-culto.page.html",
+  styleUrls: ["./novo-culto.page.scss"]
 })
 export class NovoCultoPage implements OnInit {
   protected form: FormGroup;
   //imgPath: string;
   //fileToUpload: any;
   protected fileImage: string;
-  private downloadURL: string = '';
+  protected fileImageCamera: string;
+  private downloadURL: string = "";
 
   constructor(
     private formBuilder: FormBuilder,
@@ -29,44 +30,59 @@ export class NovoCultoPage implements OnInit {
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      titulo: ['', Validators.required],
-      horario: ['', Validators.required],
-      dia: ['', Validators.required],
-      descricao: ['', Validators.required],
-      urlImagem: ['']
+      titulo: ["", Validators.required],
+      horario: ["", Validators.required],
+      dia: ["", Validators.required],
+      descricao: ["", Validators.required],
+      urlImagem: [""]
     });
   }
 
   redirecionarCultosCadastrados() {
-    this.router.navigate(['cadastro/secao/cultos']);
+    this.router.navigate(["cadastro/secao/cultos"]);
   }
 
   async salvarCulto() {
     await this.sharedModalService.presentLoadingWithOptions();
 
-   // this.downloadURL !== null ||
-   // this.downloadURL !== undefined ||
-   // this.downloadURL !== ''
-      /*?*/
-      if (this.fileImage !== undefined && this.fileImage !== null){
-        await this.uploadPicture();
-       await this.form.get('urlImagem').setValue(this.downloadURL);
-      }else{
-        await this.form.get('urlImagem').setValue('');
-      }
-
+    // this.downloadURL !== null ||
+    // this.downloadURL !== undefined ||
+    // this.downloadURL !== ''
+    /*?*/
+    if (this.fileImage !== undefined && this.fileImage !== null) {
+      await this.uploadPicture();
+      await this.form.get("urlImagem").setValue(this.downloadURL);
+    } else if (
+      this.fileImageCamera !== undefined &&
+      this.fileImageCamera !== null
+    ) {
+      await this.uploadPictureBase64();
+      await this.form.get("urlImagem").setValue(this.downloadURL);
+    } else {
+      await this.form.get("urlImagem").setValue("");
+    }
 
     await this.cultosSemanaisService.save(this.form.value).subscribe(
       success => {
-        this.sharedModalService.presentToast('Culto salvo com sucesso!', 'medium', 'custom-modal', 1500);
-        console.log('Culto salvo com sucesso!');
+        this.sharedModalService.presentToast(
+          "Culto salvo com sucesso!",
+          "medium",
+          "custom-modal",
+          1500
+        );
+        console.log("Culto salvo com sucesso!");
         this.loadingController.dismiss();
         this.resetarForm();
       },
-      error =>{ this.sharedModalService.presentToast('Erro ao salvar culto. Tente novamente!', 'danger', 'custom-modal', 1500);
-      }
-      ,
-      () => console.log('Finalizado com sucesso!')
+      error => {
+        this.sharedModalService.presentToast(
+          "Erro ao salvar culto. Tente novamente!",
+          "danger",
+          "custom-modal",
+          1500
+        );
+      },
+      () => console.log("Finalizado com sucesso!")
     );
   }
 
@@ -78,7 +94,8 @@ export class NovoCultoPage implements OnInit {
       descricao: null
     });
     this.fileImage = null;
-    this.downloadURL = '';
+    this.fileImageCamera = null;
+    this.downloadURL = "";
     this.form.get("titulo").markAsUntouched();
     this.form.get("horario").markAsUntouched();
     this.form.get("dia").markAsUntouched();
@@ -94,7 +111,21 @@ export class NovoCultoPage implements OnInit {
 
   async uploadPicture() {
     await this.firebaseService
-      .uploadPicture('imagens-culto')
+      .uploadPicture("imagens-culto")
+      .then(downURL => (this.downloadURL = downURL))
+      .catch((this.downloadURL = null));
+  }
+
+  async takePicture() {
+    this.firebaseService
+      .takePicture()
+      .then(file => (this.fileImageCamera = file))
+      .catch((this.fileImageCamera = null));
+  }
+
+  async uploadPictureBase64() {
+    await this.firebaseService
+      .uploadPictureBase64("imagens-culto")
       .then(downURL => (this.downloadURL = downURL))
       .catch((this.downloadURL = null));
   }
--- src/app/shared/services/firebase/firebase.service.ts
+++ src/app/shared/services/firebase/firebase.service.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { File } from '@ionic-native/file/ngx';
-import { AngularFireStorage } from '@angular/fire/storage';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import { environment } from '../../../../environments/environment.prod';
+import { Injectable } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { File } from "@ionic-native/file/ngx";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
+import { environment } from "../../../../environments/environment.prod";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class FirebaseService {
   public downloadURL: string;
   private path: string;
   protected fileImage: string;
   protected fileUri: string;
+  private photoBase64: string;
   private blob: Blob;
 
   constructor(
@@ -34,16 +35,16 @@ export class FirebaseService {
       await this.camera.getPicture(options).then(
         imageData => {
           this.fileUri = imageData;
-          if (this.platform.is('ios')) {
-            this.fileImage = this.fileUri.split('/').pop();
+          if (this.platform.is("ios")) {
+            this.fileImage = this.fileUri.split("/").pop();
           } else {
             this.fileImage = this.fileUri.substring(
-              this.fileUri.lastIndexOf('/') + 1,
-              this.fileUri.indexOf('?')
+              this.fileUri.lastIndexOf("/") + 1,
+              this.fileUri.indexOf("?")
             );
           }
           this.path =
-            this.fileUri.substring(0, this.fileUri.lastIndexOf('/')) + '/';
+            this.fileUri.substring(0, this.fileUri.lastIndexOf("/")) + "/";
         },
         err => {
           //alert(err);
@@ -56,29 +57,64 @@ export class FirebaseService {
     return await this.fileImage;
   }
 
-  async takePhoto(){
+  async takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: true
+    };
 
+    await this.camera
+      .getPicture(options)
+      .then(
+        async ImageData => {
+          //let base64Image = 'data:image/jpeg;base64,' + ImageData;
+          this.photoBase64 = ImageData;
+        },
+        err => {
+          alert(err);
+        }
+      )
+      .catch(error => {
+        alert(error);
+      });
+    return "IMG-" + Math.random() * 1000000;
+  }
+
+  async uploadPictureBase64(pasta: string) {
+    const filename = (await environment.guid.raw().toString()) + ".jpg";
+    const ref = await this.afStorage.ref(pasta + "/" + filename);
+    const task = await ref.putString(this.photoBase64, "base64", {
+      contentType: "image/jpg"
+    });
+    await task.ref.getDownloadURL().then(urlDownload => {
+      this.downloadURL = urlDownload;
+    });
+    await this.resetarDados();
+    return await this.downloadURL;
   }
 
   async uploadPicture(pasta: string) {
     if (
-      await this.path === '' ||
-      await this.path === null ||
-      await this.path === undefined ||
-      await this.fileImage === '' ||
-      await this.fileImage === null ||
-      await this.fileImage === undefined
+      (await this.path) === "" ||
+      (await this.path) === null ||
+      (await this.path) === undefined ||
+      (await this.fileImage) === "" ||
+      (await this.fileImage) === null ||
+      (await this.fileImage) === undefined
     ) {
       return;
     } else {
-      const filename = environment.guid.raw().toString() + '.jpg';
+      const filename = environment.guid.raw().toString() + ".jpg";
       //alert(filename);
       const buffer: ArrayBuffer = await this.file.readAsArrayBuffer(
         this.path,
         this.fileImage
       );
-      this.blob = await new Blob([buffer], { type: 'image/jpeg' });
-      const ref = await this.afStorage.ref(pasta + '/' + filename);
+      this.blob = await new Blob([buffer], { type: "image/jpeg" });
+      const ref = await this.afStorage.ref(pasta + "/" + filename);
       const task = await ref.put(this.blob);
       await task.ref.getDownloadURL().then(urlDownload => {
         this.downloadURL = urlDownload;
@@ -92,14 +128,16 @@ export class FirebaseService {
     this.path = await null;
     this.fileImage = await null;
     this.blob = await null;
+    this.photoBase64 = await null;
   }
 
-  async deletarImagemStorage(pasta: string, url: string){
+  async deletarImagemStorage(pasta: string, url: string) {
     let imagemDeletar = await url.substring(
-              url.indexOf('%') + 3,
-              url.indexOf('?'));
-            console.log(pasta + imagemDeletar);
-             await this.afStorage.ref(pasta + '/' + imagemDeletar).delete();
+      url.indexOf("%") + 3,
+      url.indexOf("?")
+    );
+    console.log(pasta + imagemDeletar);
+    await this.afStorage.ref(pasta + "/" + imagemDeletar).delete();
     return;
   }
 }
